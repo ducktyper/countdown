abc6d7f4b81e0537fbc320191d55e833f6d07bc8
ducksan cho <daniel.cho.programmer@gmail.com>
2015-07-21 20:46:16 +1200

    Fix typo

--- store_test.rb
+++ store_test.rb
@@ -16,32 +16,32 @@ describe "store" do
     assert_equal(2, store.product_count())
   end
 
-  it "calculate cost" do
+  it "calculates cost" do
     store = Store.new()
     store.add_product("0001", "apple", 5)
     assert_equal(5, store.calculate_cost(["0001"]))
   end
 
-  it "add duplicated named products" do
+  it "adds duplicated named products" do
     store = Store.new()
     store.add_product("0001", "apple", 5)
     store.add_product("0003", "apple", 10)
     assert_equal(15, store.calculate_cost(["0001", "0003"]))
   end
 
-  it "add duplicated barcode replace properties" do
+  it "adds duplicated barcode replace properties" do
     store = Store.new()
     store.add_product("0001", "apple", 5)
     store.add_product("0001", "orange", 10)
-    expact = "orange $10\ntotal $10"
-    assert_equal(expact, store.print_receipt(["0001"]))
+    expect = "orange $10\ntotal $10"
+    assert_equal(expect, store.print_receipt(["0001"]))
   end
 
-  it "print receipt" do
+  it "prints receipt" do
     store = Store.new()
     store.add_product("0001", "apple", 5)
-    expact = "apple $5\ntotal $5"
-    assert_equal(expact, store.print_receipt(["0001"]))
+    expect = "apple $5\ntotal $5"
+    assert_equal(expect, store.print_receipt(["0001"]))
   end
 
 end
