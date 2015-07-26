e6032a97d17714d1f9decffe3703d37f573a3495
ducksan cho <daniel.cho.programmer@gmail.com>
2015-07-26 18:21:10 +1200

    extract db codes

---
+++ db_helper.rb
@@ -0,0 +1,33 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+ActiveRecord::Schema.define do
+
+  create_table "products" do |t|
+    t.string   "barcode",       limit: 255
+    t.string   "name",          limit: 255
+    t.decimal  "cost",          precision: 8, scale: 2
+  end
+
+  create_table "discounts" do |t|
+    t.integer  "product_id",    limit: 4
+    t.decimal  "amount",        precision: 8, scale: 2
+  end
+
+  create_table "purchases" do |t|
+    t.integer  "product_id",    limit: 4
+    t.decimal  "amount",        precision: 8, scale: 2
+    t.datetime "purchased_at"
+  end
+
+  create_table "products_purchases", id: false do |t|
+    t.integer  "product_id",    limit: 4
+    t.integer  "purchase_id",   limit: 4
+  end
+
+  create_table "discounts_purchases", id: false do |t|
+    t.integer  "discount_id",   limit: 4
+    t.integer  "purchase_id",   limit: 4
+  end
+
+end
--- test_helper.rb
+++ test_helper.rb
@@ -1,33 +1,6 @@
 require 'minitest/autorun'
-require 'active_record'
+require 'db_helper'
 
 # Use travel_to
 require 'active_support/testing/time_helpers'
 include ActiveSupport::Testing::TimeHelpers
-
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
-ActiveRecord::Schema.define do
-  create_table "products" do |t|
-    t.string   "barcode",       limit: 255
-    t.string   "name",          limit: 255
-    t.decimal  "cost",          precision: 8, scale: 2
-  end
-  create_table "discounts" do |t|
-    t.integer  "product_id",    limit: 4
-    t.decimal  "amount",        precision: 8, scale: 2
-  end
-  create_table "purchases" do |t|
-    t.integer  "product_id",    limit: 4
-    t.decimal  "amount",        precision: 8, scale: 2
-    t.datetime "purchased_at"
-  end
-  create_table "products_purchases", id: false do |t|
-    t.integer  "product_id",    limit: 4
-    t.integer  "purchase_id",   limit: 4
-  end
-  create_table "discounts_purchases", id: false do |t|
-    t.integer  "discount_id",   limit: 4
-    t.integer  "purchase_id",   limit: 4
-  end
-
-end
